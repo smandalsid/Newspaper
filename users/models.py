c070d282ee6c5ef67d2693b65126e7b7035b02f7
smandalsid <smandal.sid@gmail.com>
2021-04-17 01:03:34 +0530

    first commit

---
+++ users/models.py
@@ -0,0 +1,9 @@
+# users/models.py
+
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+# Create your models here.
+
+class CustomUser(AbstractUser):
+    age=models.PositiveIntegerField(null=True, blank=True)
